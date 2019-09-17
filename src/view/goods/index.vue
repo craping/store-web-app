3a976be2c28e4515f487187e6220ff09fad4f259
zoulw <zoulw@yunjiglobal.com>
2019-09-17 23:54:34 +0800

    商品评价

--- src/view/goods/index.vue
+++ src/view/goods/index.vue
@@ -19,7 +19,7 @@
         <van-icon name="arrow-left" color="#999" size="20" @click="left('in')"/>
       </template>
       <template v-slot:title>
-        <van-tabs v-model="activeTab" line-width="40" :border="false" @click="scrollToAnchor">
+        <van-tabs v-model="activeTab" :class="{'visibility':showJudgeSheet}" line-width="40" :border="false" @click="scrollToAnchor">
           <van-tab ref="tab-product" title="商品" name="product"></van-tab>
           <van-tab ref="tab-comment" title="评价" name="comment"></van-tab>
           <van-tab ref="tab-detail" title="详情" name="detail"></van-tab>
@@ -58,7 +58,7 @@
     </van-cell-group>
 
     <van-cell-group class="goods-cell-group">
-      <van-cell title-class="title" title="7天退换.免费包邮" value="" :is-link="true">
+       <van-cell title-class="title" title="7天退换.免费包邮" value="" @click="showServiceSheet = true" :is-link="true">
         <template v-slot:icon class="title">服务：</template>
       </van-cell>
       <van-cell  title-class="title" title="7天退换.免费包邮" value="" :is-link="true">
@@ -69,26 +69,27 @@
       </van-cell>
     </van-cell-group>
 
-    <van-cell-group class="goods-cell-group" ref="comment">
-      <van-cell title="商品评价" />
-      <van-cell value="单元格" icon="shop-o" is-link @click="sorry" v-for="i in 5" :key="i">
-        <template slot="title">
-          <span class="van-cell-text"></span>
-          <van-tag class="goods-tag" type="danger">标签</van-tag>
-        </template>
-      </van-cell>
-      <van-cell title="线下门店" icon="location-o" is-link @click="sorry" />
+    <van-cell-group class="goods-cell-group" ref="comment" @click="showJudge"> 
+      <div class="judge-top van-hairline--bottom">
+        <div>商品评价(1111)</div>
+        <div class="right">查看全部<van-icon name="arrow" color="#f44"/></div>
+      </div>
+      <div class="judge-area">
+        <div class="row-1">
+          <img class="head-img" src="">
+          <span class="user-name">名**字</span>
+        </div>
+        <div class="row-2 overhide">
+          很好看很好看很好看很好看很好看，很好看很好看很好看很好看，很好看很好看很好看很好很好看很好看很好看，很好看很好看很好看很好看很好看很好看很好看很好看很好看很好看很好看很好看很好看
+        </div>
+      </div>
     </van-cell-group>
-    
     <van-cell-group class="goods-cell-group" ref="detail">
       <van-cell title="图文详细" @click="sorry" />
-      <van-cell value="单元格" icon="shop-o" is-link @click="sorry" v-for="i in 20" :key="i">
-        <template slot="title">
-          <span class="van-cell-text"></span>
-          <van-tag class="goods-tag" type="danger">标签</van-tag>
-        </template>
-      </van-cell>
-      <van-cell title="线下门店" icon="location-o" is-link @click="sorry" />
+      <div class="goods-detail" >
+        <img v-for="j in 4" src="https://img.yzcdn.cn/vant/apple-1.jpg" @click="showPre">
+      </div>
+      
     </van-cell-group>
     
     <store-share :show="share.show" @cancel="share.show=false"></store-share>
@@ -107,6 +108,40 @@
         立即购买
       </van-goods-action-button>
     </van-goods-action>
+  
+    <van-popup
+      class="judge-popup"
+      v-model="showJudgeSheet"
+      position="right"
+    >
+      <judge-sheet @showPre="showPre"></judge-sheet>
+    </van-popup>
+    <van-popup
+      class="service-popup"
+      v-model="showServiceSheet"
+      position="bottom"
+    >
+      <div class="service-content">
+        <div class="title">服务说明</div>
+        <div class="item-wrapper">
+          <div class="item" v-for='i in 5'>
+          <div class="left"><van-icon name="certificate" color="#f44" /></div>
+          <div class="right">
+            <div class="itme-title">正品保证</div>
+            <div class="itme-info">本商品是滴是滴时间来得及啊第四季度是本商品是滴是滴时间来得及啊第四季度是</div>
+          </div>
+        </div>
+        </div>
+        <div class="btn" @click="showServiceSheet=false">完成</div>
+      </div>
+    </van-popup>
+
+    <van-image-preview
+      v-model="prePicShow"
+      :images="preImage"
+      :show-index='false'
+      >
+    </van-image-preview>
   </div>
 </template>
 
@@ -125,12 +160,15 @@ import {
   GoodsActionButton,
   ImagePreview,
   Tab,
-  Tabs
+  Tabs,
+  Popup
 } from 'vant';
 import storeNavBar from "@/components/store-nav-bar";
 import storeShare from "@/components/store-share";
+import judgeSheet from "./judgeSheet";
 import Big from "big.js";
-
+import Vue from 'vue';
+Vue.use(ImagePreview);
 export default {
   components: {
     [Tag.name]: Tag,
@@ -146,8 +184,10 @@ export default {
     [ImagePreview.name]: ImagePreview,
     [Tab.name]:Tab,
     [Tabs.name]:Tabs,
+    [Popup.name]:Popup,
     storeNavBar,
-    storeShare
+    storeShare,
+    judgeSheet
   },
 
   data() {
@@ -173,7 +213,11 @@ export default {
           'http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/images/20180615/5ac98b64N70acd82f.jpg!cc_350x449.jpg',
           'http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/images/20180615/5a51eb88Na4797877.jpg'
         ]
-      }
+      },
+      showJudgeSheet: false,
+      prePicShow: false,
+      preImage: [],
+      showServiceSheet: false,      
     };
   },
   created(){
@@ -189,6 +233,7 @@ export default {
   methods: {
     handleScroll(e){
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+      if(this.showJudgeSheet)return
       this.opacityIn = scrollTop/(this.$refs.product.offsetTop- 66);
       this.opacityOut = 1 - (scrollTop/(this.$refs.product.offsetTop - 66));
 
@@ -225,7 +270,14 @@ export default {
       }, 500);
     },
     left(e){
-      this.$router.replace("/main/home");
+      console.log('ss',this.showJudgeSheet)
+      if(this.showJudgeSheet) {
+        this.showJudgeSheet = false
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        this.opacityIn = scrollTop/(this.$refs.product.offsetTop- 66);
+      } else {
+        this.$router.replace("/main/home");
+      }
     },
     onSwipeChange(index) {
       this.swipe.current = index;
@@ -246,6 +298,16 @@ export default {
 
     sorry() {
       Toast('暂无后续逻辑~');
+    },
+    showJudge() {
+      this.opacityIn = 1
+      this.showJudgeSheet = true
+    },
+    showPre(img) {
+      this.preImage = [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+      ]
+      this.prePicShow = true
     }
   }
 };
@@ -349,6 +411,133 @@ export default {
 
   &-tag {
     margin-left: 5px;
+  }
+    .visibility{
+    visibility: hidden;
+  }
+  .judge-top{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 16px;
+    font-size: 14px;
+    .right{
+      color: $red;
+      display: flex;
+      align-items: center;
+      font-size: 12px;
+    }
+  }
+  .judge-area{
+    min-height: 100px;
+    padding: 15px;
+    &.inPopup{
+      margin: 10px;
+      border-radius: 4px;
+      overflow: hidden;
+      background: #FFF;
+    }
+    .row-1{
+      display: flex;
+      align-items: center;
+      .head-img{
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background: #eee;
+      }
+      .user-name{
+        color: #999;
+      }
+    }
+    .row-2{
+      margin-top: 10px;
+      &.overhide{
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+      }
+      .pic-area{
+        margin-top: 10px;
+        img{
+          width: 70px;
+          height: 70px;
+          margin-right: 10px;
+        }
+      }
+    }
+    .row-3{
+      margin-top: 10px;
+      color: #999;
+    }
+  }
+  .van-image-preview__overlay{
+    z-index: 3100 !important;
+  }
+  .van-image-preview{
+    z-index: 3120 !important;
+  }
+  .judge-popup{
+    height: 100%;
+    width: 100%;
+    padding-top: 76px;
+    background: #f2f2f2;
+  }
+  .service-popup{
+    height: 50%;
+    border-top-left-radius: 14px;
+    border-top-right-radius: 14px;
+  }
+  .goods-detail{
+    img{
+      width: 100%;
+    }
+  }
+  .service-content{
+    padding: 0 15px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    .title{
+      margin: 15px 0;
+      font-size: 16px;
+      text-align: center;
+      color: #000;
+    }
+    .item-wrapper{
+      flex: 1;
+      overflow: auto;
+    }
+    .item{
+      display: flex;
+      align-items: flex-start;
+      margin-bottom: 15px;
+      .left{
+        margin: 3px 5px 0 0;
+      }
+      .itme-title{
+        font-size: 14px;
+      }
+      .itme-info{
+        margin-top: 4px;
+        color: #999;
+      }
+      
+    }
+    .btn{
+      width: 95%;
+      background: $red;
+      text-align: center;
+      height: 40px;
+      line-height: 40px;
+      color: #FFF;
+      font-size: 16px;
+      margin: 0 auto;
+      flex-basis: 0;
+      border-radius: 20px;
+    }
   }
 }
 </style>
