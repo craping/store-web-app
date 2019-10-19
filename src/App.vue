4c5152c1232dcc7c0470459ee4749986648b8ad4
zoulw <zoulw@yunjiglobal.com>
2019-10-19 16:20:59 +0800

    还原微信授权链接

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,7 @@ export default {
       if(getClientId() == 3 && !getToken()){
           //微信中
           const appid = 'wx455d4595caac679c'
-          const returnUrl = encodeURIComponent(location.href)
-          const redirect_uri =  encodeURIComponent(`http://m.5gyungou.com/wx.html?returnUrl=${returnUrl}`) 
+          const redirect_uri =  encodeURIComponent(`http://m.5gyungou.com/wx.html?returnUrl=${location.href}`) 
           window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=snsapi_userinfo&connect_redirect=1#wechat_redirect`
         }
     });
