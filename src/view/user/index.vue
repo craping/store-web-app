4b8d87e0ea62b187de7377e3b738a9f6dea5a3f9
zoulw <zoulw@yunjiglobal.com>
2019-10-27 18:52:39 +0800

    补充提交

--- src/view/user/index.vue
+++ src/view/user/index.vue
@@ -49,11 +49,7 @@
           </div>
         </div>
       </div>
-<<<<<<< HEAD
-      <div class="no-vip-card" @click="jumpLink('/vipGrade')">
-=======
       <div v-else-if="!isVip && VIP_ENABLE" class="no-vip-card" @click="jumpLink('/vipGrade')">
->>>>>>> 07ce40f45fda31c833eaca9215d931e478d910e3
         成为<span class="vip-grade-bar">会员</span>享受更多优惠
       </div>
     </div>
